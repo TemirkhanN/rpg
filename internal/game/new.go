3465f93fb990c6e4b19efc2a337f502df1bd141b
Temirkhan <temirkhan@nasukhov.me>
2021-10-31 13:10:46 +0100

    Added dumb-straightforward way to interact with npcs

--- internal/game/new.go
+++ internal/game/new.go
@@ -3,6 +3,7 @@ package game
 import (
 	"fmt"
 	"os"
+	"unicode/utf8"
 
 	"github.com/gdamore/tcell"
 
@@ -10,29 +11,43 @@ import (
 )
 
 type Game struct {
-	player    *rpg.Player
+	player    *player
 	locations []rpg.Location
+	npcs      []*npc
 
-	title    *text
-	status   *playerPanel
-	teleport *teleportPanel
+	screen tcell.Screen
 }
 
-type playerPanel struct {
-	panel    *box
-	name     *text
-	location *text
+type npc struct {
+	asci asci
+	npc  rpg.NPC
+	pos  position
 }
 
-type teleportPanel struct {
-	panel     *box
-	locations []text
+type player struct {
+	asci            asci
+	player          *rpg.Player
+	pos             position
+	currentDialogue string
+}
+
+type asci struct {
+	symbol rune
+	style  tcell.Style
 }
 
 func New(playerName string) *Game {
 	newGame := new(Game)
 	newPlayer := rpg.NewPlayer(playerName)
-	newGame.player = &newPlayer
+	newGame.player = &player{
+		asci: asci{
+			symbol: '🐶',
+			style:  playerStyle,
+		},
+		currentDialogue: "",
+		player:          &newPlayer,
+		pos:             position{x: 30, y: 15},
+	}
 
 	newGame.locations = []rpg.Location{
 		rpg.NewLocation("Gludio"),
@@ -40,87 +55,163 @@ func New(playerName string) *Game {
 		rpg.NewLocation("Goddard"),
 	}
 
-	newNPC := rpg.NewNPC("Ded")
-	fmt.Println(newPlayer.StartConversation(newNPC))
+	newGame.npcs = []*npc{
+		{
+			asci: asci{style: friendlyNPCStyle, symbol: '👱'},
+			npc:  rpg.NewNPC("Newbie helper"),
+			pos:  position{x: 40, y: 8},
+		},
+		{
+			asci: asci{style: friendlyNPCStyle, symbol: '🧔'},
+			npc:  rpg.NewNPC("Ded"),
+			pos:  position{x: 55, y: 13},
+		},
+	}
+
+	newGame.screen = createScreen()
+	newGame.screen.EnableMouse()
 
 	return newGame
 }
 
 func (g *Game) Start() {
-	screen := createScreen()
-	screen.EnableMouse()
-	width, _ := screen.Size()
+	g.drawGraphics()
+	for {
+		g.handleEvents()
+	}
+}
+
+func (g Game) drawGraphics() {
+	g.screen.Clear()
+	width, _ := g.screen.Size()
 	screenCenter := width / 2
 
-	g.title = newText("RPG maker", screenCenter-10, 1)
-	g.title = newText("RPG maker", screenCenter-10, 1)
-	g.status = &playerPanel{
-		panel:    newBox(1, 1, 25, 10, "Status"),
-		name:     newText("Name: "+g.player.Name(), 2, 3),
-		location: newText("Currently at: "+g.player.Whereabouts().Name(), 2, 4),
-	}
+	// Game title.
+	newText("RPG maker", screenCenter-10, 1).draw(g.screen, infoTextStyle)
 
-	g.teleport = &teleportPanel{
-		panel:     newBox(1, 12, 25, 20, "Move to"),
-		locations: make([]text, len(g.locations)),
-	}
-
-	for i, location := range g.locations {
-		g.teleport.locations[i] = *newText(
-			fmt.Sprintf("%d.%s", i+1, location.Name()),
-			g.teleport.panel.leftTop.x+1,
-			g.teleport.panel.leftTop.y+i+1,
-		)
-	}
-
-	g.drawGraphics(screen)
-	for {
-		g.handleEvents(screen)
-	}
-}
-
-func (g Game) drawGraphics(screen tcell.Screen) {
-	screen.Clear()
-
-	g.title.draw(screen, infoTextStyle)
-
-	// Player status panel.
-	g.status.panel.draw(screen, boxStyle)
-	g.status.name.draw(screen, infoTextStyle)
-	g.status.location.draw(screen, infoTextStyle)
+	// Status panel.
+	newBox(1, 1, 25, 10, "Status").draw(g.screen, boxStyle)
+	newText("Name: "+g.player.player.Name(), 2, 3).draw(g.screen, infoTextStyle)
+	newText("Currently at: "+g.player.player.Whereabouts().Name(), 2, 4).draw(g.screen, infoTextStyle)
 
 	// Teleport panel.
-	g.teleport.panel.draw(screen, boxStyle)
-	for _, location := range g.teleport.locations {
-		location.draw(screen, infoTextStyle)
+	newBox(1, 12, 25, 20, "Move to").draw(g.screen, boxStyle)
+	for i, location := range g.locations {
+		newText(fmt.Sprintf("%d.%s", i+1, location.Name()), 1+1, 12+1+i).draw(g.screen, infoTextStyle)
 	}
 
-	screen.Show()
+	// Draw npcs.
+	for _, gameNpc := range g.npcs {
+		newText(string(gameNpc.asci.symbol), gameNpc.pos.x, gameNpc.pos.y).draw(g.screen, gameNpc.asci.style)
+	}
+
+	// Draw player.
+	newText(string(g.player.asci.symbol), g.player.pos.x, g.player.pos.y).draw(g.screen, g.player.asci.style)
+
+	// Draw player dialogue panel.
+	if g.player.currentDialogue != "" {
+		newBox(80, 12, 110, 20, "Dialogue").draw(g.screen, boxStyle)
+		verticalOffset := 13
+
+		charactersPerLine := 110 - 80 - 4
+		dialogueLength := utf8.RuneCountInString(g.player.currentDialogue)
+		if dialogueLength <= charactersPerLine {
+			newText(g.player.currentDialogue, 82, verticalOffset).draw(g.screen, textStyle)
+		} else {
+			var linedString string
+			for i, r := range g.player.currentDialogue {
+				linedString += string(r)
+
+				if (i+1)%charactersPerLine == 0 || i+1 == dialogueLength {
+					newText(linedString, 82, verticalOffset).draw(g.screen, textStyle)
+					verticalOffset++
+					linedString = ""
+				}
+			}
+		}
+	}
+
+	g.screen.Show()
 }
 
-func (g Game) handleEvents(screen tcell.Screen) {
-	switch ev := screen.PollEvent().(type) {
+func (g Game) handleEvents() {
+	switch ev := g.screen.PollEvent().(type) {
 	case *tcell.EventKey:
-		if ev.Key() == tcell.KeyEscape {
-			screen.Fini()
-			os.Exit(0)
-		}
+		g.handleControlsInput(ev.Key())
 	case *tcell.EventMouse:
 		// left click
 		if ev.Buttons() == tcell.Button1 {
 			mouseX, mouseY := ev.Position()
-			teleport := g.teleport.panel
 
-			if (mouseX >= teleport.leftTop.x+1 && mouseX <= teleport.rightBottom.x-1) &&
-				(mouseY >= teleport.leftTop.y+1 && mouseY <= teleport.leftTop.y+len(g.locations)) {
-				locationKey := mouseY - teleport.leftTop.y - 1
-				if g.player.Whereabouts().Name() != g.locations[locationKey].Name() {
-					g.player.MoveToLocation(g.locations[locationKey])
-					g.status.location.text = fmt.Sprintf("Currently at: %s", g.player.Whereabouts().Name())
+			// Handle click on teleport panel.
+			if (mouseX >= 1+1 && mouseX <= 25-1) &&
+				(mouseY >= 12+1 && mouseY <= 12+len(g.locations)) {
+				locationKey := mouseY - 12 - 1
+				if g.player.player.Whereabouts().Name() != g.locations[locationKey].Name() {
+					g.player.player.MoveToLocation(g.locations[locationKey])
 
-					g.drawGraphics(screen)
+					g.drawGraphics()
 				}
 			}
 		}
 	}
 }
+
+func (g Game) handleControlsInput(input tcell.Key) {
+	if input == tcell.KeyEscape {
+		g.screen.Fini()
+		os.Exit(0)
+	}
+
+	if input == tcell.KeyUp {
+		newPosition := g.player.pos
+		newPosition.y--
+
+		g.playerMoveTo(newPosition)
+	}
+
+	if input == tcell.KeyDown {
+		newPosition := g.player.pos
+		newPosition.y++
+
+		g.playerMoveTo(newPosition)
+	}
+
+	if input == tcell.KeyLeft {
+		newPosition := g.player.pos
+		newPosition.x--
+
+		g.playerMoveTo(newPosition)
+	}
+
+	if input == tcell.KeyRight {
+		newPosition := g.player.pos
+		newPosition.x++
+
+		g.playerMoveTo(newPosition)
+	}
+}
+
+func (g Game) playerMoveTo(to position) {
+	movementAllowed := true
+	for _, npc := range g.npcs {
+		if npc.pos == to {
+			dialogue := g.player.player.StartConversation(npc.npc)
+			g.player.currentDialogue = dialogue
+			movementAllowed = false
+			g.drawGraphics()
+
+			break
+		}
+	}
+
+	if movementAllowed {
+		g.player.pos = to
+		g.drawGraphics()
+	}
+}
+
+var (
+	friendlyNPCStyle = tcell.StyleDefault.Background(tcell.ColorWhite).Foreground(tcell.ColorGreen).Bold(true)
+	playerStyle      = tcell.StyleDefault.Background(tcell.ColorBlack).Foreground(tcell.ColorGoldenrod).Bold(true)
+)
